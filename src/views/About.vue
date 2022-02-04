bd779a40f99138d6073c66932cdb5bca2083f07f
MaryahKim <maryapitaz@gmail.com>
2022-02-04 18:09:28 +0300

    Update About.vue
    update h1

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus tenetur consequatur quam deleniti, qui eius dicta expedita, maxime necessitatibus culpa et? Mollitia, exercitationem laborum placeat impedit illum recusandae blanditiis nisi?</h1>
   </div>
 </template>
